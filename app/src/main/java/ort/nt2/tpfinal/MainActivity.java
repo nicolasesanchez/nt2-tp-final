5a7d0a675bf313aa413058b89b59b3a96b8bf295
Cecilia Quintana <ceciquin@protonmail.ch>
2019-11-20 19:55:25 -0300

    pantalla orders

--- app/src/main/java/ort/nt2/tpfinal/MainActivity.java
+++ app/src/main/java/ort/nt2/tpfinal/MainActivity.java
@@ -38,12 +38,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn = (Button) findViewById(R.id.orders);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button btn2 = (Button) findViewById(R.id.orders);
+        /*btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, OrdersActivity.class));
+                Intent intent = new Intent(MainActivity.this,OrdersActivity.class);
+                 startActivity(intent);
             }
-        });
+        });*/
     }
+
+    public void goToOrder(View v){
+        Intent intent = new Intent(MainActivity.this,OrdersActivity.class);
+        startActivity(intent);
+    }
+
 }
