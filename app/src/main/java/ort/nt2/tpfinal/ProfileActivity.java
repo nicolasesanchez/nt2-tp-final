b66e3b817b98214e07f813d73ea230219b5b11f2
nicolasesanchez <nicolase.sanchez@mercadolibre.com>
2019-11-20 18:04:58 -0300

    remove unused things

--- app/src/main/java/ort/nt2/tpfinal/ProfileActivity.java
+++ app/src/main/java/ort/nt2/tpfinal/ProfileActivity.java
@@ -12,16 +12,13 @@ import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
