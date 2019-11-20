d1314c09117d9cf2f57281f88687238983b81d01
nicolasesanchez <nicolase.sanchez@mercadolibre.com>
2019-11-20 19:52:38 -0300

    client screen

---
+++ app/src/main/java/ort/nt2/tpfinal/ClientActivity.java
@@ -0,0 +1,54 @@
+package ort.nt2.tpfinal;
+
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ort.nt2.tpfinal.adapters.ClientAdapter;
+import ort.nt2.tpfinal.entities.Client;
+import ort.nt2.tpfinal.sql.SQLiteHelper;
+
+public class ClientActivity extends ListActivity {
+    private ListView clientsList;
+    private ClientAdapter clientAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clients_list);
+
+        clientsList = (ListView) findViewById(android.R.id.list);
+        List<Client> clients = getClients();
+
+        clientAdapter = new ClientAdapter(this, clients);
+        clientsList.setAdapter(clientAdapter);
+
+
+        /*FragmentManager fm = getSupportFragmentManager();
+        ClientModal editNameDialogFragment = new ClientModal();
+        editNameDialogFragment.show(fm, "fragment_edit_name");*/
+
+    }
+
+    private List<Client> getClients() {
+        Cursor cursor = SQLiteHelper.getInstance(this).getReadableDatabase().rawQuery("select dni, name, last_name from client", new String[]{});
+
+        ArrayList<Client> clients = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            clients.add(new Client(
+                    cursor.getInt(cursor.getColumnIndex("dni")),
+                    cursor.getString(cursor.getColumnIndex("name")),
+                    cursor.getString(cursor.getColumnIndex("last_name"))
+            ));
+        }
+
+        return clients;
+    }
+
+}
---
+++ app/src/main/java/ort/nt2/tpfinal/adapters/ClientAdapter.java
@@ -0,0 +1,54 @@
+package ort.nt2.tpfinal.adapters;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import ort.nt2.tpfinal.R;
+import ort.nt2.tpfinal.entities.Client;
+
+public class ClientAdapter extends ArrayAdapter<Client> {
+    private Context context;
+    private List<Client> clientsList;
+
+    public ClientAdapter(@NonNull Context context, @LayoutRes List<Client> list) {
+        super(context, 0, list);
+        this.context = context;
+        clientsList = list;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View clientsList = convertView;
+
+        if (clientsList == null) clientsList = LayoutInflater.from(context).inflate(R.layout.row_client, parent, false);
+
+        Client currentClient = this.clientsList.get(position);
+
+        TextView clientFullName = (TextView) clientsList.findViewById(R.id.client_name);
+        clientFullName.setText((String.format("%s %s", currentClient.getName(), currentClient.getLastName())));
+        clientFullName.setPadding(15, 5, 0, 5);
+
+        TextView clientPersonalId = (TextView) clientsList.findViewById(R.id.client_id);
+        clientPersonalId.setText(parsePersonalId(currentClient.getPersonalId()));
+        clientPersonalId.setPadding(0, 5, 5, 15);
+
+        return clientsList;
+    }
+
+    private String parsePersonalId(int dni) {
+        String dniString = String.valueOf(dni);
+        return String.format("%s.%s.%s", dniString.substring(0,2), dniString.substring(2, 5), dniString.substring(5, 8));
+    }
+
+}
